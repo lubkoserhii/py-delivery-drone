a014fd77e6b367dcb42f50b3580a94c8d3cd257f
lubkoserhii <lubko.serhii@gmail.com.com>
2026-03-31 21:33:02 +0300

    fixed issues with coords

--- app/main.py
+++ app/main.py
@@ -12,8 +12,7 @@ class BaseRobot:
     ) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords \
-            if coords is not None else [0, 0]
+        self.coords = coords or [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -41,8 +40,7 @@ class FlyingRobot(BaseRobot):
         super().__init__(
             name,
             weight,
-            coords
-            if coords is not None else [0, 0, 0]
+            coords=coords or [0, 0, 0]
         )
 
     def go_up(self, step: int = 1) -> None:
